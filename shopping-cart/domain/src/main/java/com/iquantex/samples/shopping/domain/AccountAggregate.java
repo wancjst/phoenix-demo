6b59648ae92ac1a9d2a7cd5b2ecc5178ead04c07
quail <823726926@qq.com>
2021-11-16 21:02:25 +0800

    part-2:使用分布式事务处理

--- shopping-cart/domain/src/main/java/com/iquantex/samples/shopping/domain/AccountAggregate.java
+++ shopping-cart/domain/src/main/java/com/iquantex/samples/shopping/domain/AccountAggregate.java
@@ -5,10 +5,20 @@ import com.iquantex.phoenix.server.aggregate.ActReturn;
 import com.iquantex.phoenix.server.aggregate.CommandHandler;
 import com.iquantex.phoenix.server.aggregate.EntityAggregateAnnotation;
 import com.iquantex.phoenix.server.aggregate.QueryHandler;
-import com.iquantex.samples.shopping.account.AccountQueryCmd;
-import com.iquantex.samples.shopping.account.AccountQueryEvent;
-import com.iquantex.samples.shopping.buyGoods.BuyGoodsCmd;
-import com.iquantex.samples.shopping.buyGoods.BuyGoodsEvent;
+import com.iquantex.samples.shopping.coreapi.account.AccountCancelCmd;
+import com.iquantex.samples.shopping.coreapi.account.AccountCancelOkEvent;
+import com.iquantex.samples.shopping.coreapi.account.AccountConfirmCmd;
+import com.iquantex.samples.shopping.coreapi.account.AccountConfirmOkEvent;
+import com.iquantex.samples.shopping.coreapi.account.AccountPayCmd;
+import com.iquantex.samples.shopping.coreapi.account.AccountPayCompensateCmd;
+import com.iquantex.samples.shopping.coreapi.account.AccountPayCompensateOkEvent;
+import com.iquantex.samples.shopping.coreapi.account.AccountPayFailEvent;
+import com.iquantex.samples.shopping.coreapi.account.AccountPayOkEvent;
+import com.iquantex.samples.shopping.coreapi.account.AccountQueryCmd;
+import com.iquantex.samples.shopping.coreapi.account.AccountQueryEvent;
+import com.iquantex.samples.shopping.coreapi.account.AccountTryCmd;
+import com.iquantex.samples.shopping.coreapi.account.AccountTryFailEvent;
+import com.iquantex.samples.shopping.coreapi.account.AccountTryOkEvent;
 
 import java.io.Serializable;
 
@@ -36,8 +46,77 @@ public class AccountAggregate implements Serializable {
 	}
 
 	@CommandHandler(aggregateRootId = "accountCode")
-	public ActReturn act(BuyGoodsCmd cmd){
-		return ActReturn.builder().retCode(RetCode.SUCCESS)
-				.event(new BuyGoodsEvent("ok")).build();
+	public ActReturn act(AccountTryCmd cmd) {
+		if (amt - frozenAmt > cmd.getFrozenAmt()) {
+			return ActReturn.builder().retCode(RetCode.SUCCESS)
+					.event(new AccountTryOkEvent(cmd.getAccountCode(), cmd.getFrozenAmt())).build();
+		}
+		else {
+			String retMessage = String.format("资金可用不足，剩余:%f, 当前需要冻结:%f", amt - frozenAmt, cmd.getFrozenAmt());
+			return ActReturn.builder().retCode(RetCode.FAIL).retMessage(retMessage)
+					.event(new AccountTryFailEvent(cmd.getAccountCode(), cmd.getFrozenAmt(), retMessage)).build();
+		}
 	}
+
+	public void on(AccountTryOkEvent event) {
+		frozenAmt += event.getFrozenAmt();
+	}
+
+	public void on(AccountTryFailEvent event) {
+	}
+
+	@CommandHandler(aggregateRootId = "accountCode")
+	public ActReturn act(AccountConfirmCmd cmd) {
+		return ActReturn.builder().retCode(RetCode.SUCCESS)
+				.event(new AccountConfirmOkEvent(cmd.getAccountCode(), cmd.getFrozenAmt())).build();
+	}
+
+	public void on(AccountConfirmOkEvent event) {
+		amt -= event.getFrozenAmt();
+		frozenAmt -= event.getFrozenAmt();
+	}
+
+	@CommandHandler(aggregateRootId = "accountCode")
+	public ActReturn act(AccountCancelCmd cmd) {
+		return ActReturn.builder().retCode(RetCode.SUCCESS)
+				.event(new AccountCancelOkEvent(cmd.getAccountCode(), cmd.getFrozenAmt())).build();
+	}
+
+	public void on(AccountCancelOkEvent event) {
+		frozenAmt -= event.getFrozenAmt();
+	}
+
+	@CommandHandler(aggregateRootId = "accountCode")
+	public ActReturn act(AccountPayCmd cmd) {
+		if (cmd.getAmt() < 0) {
+			throw new RuntimeException("金额不能小于0");
+		}
+		if (amt > cmd.getAmt()) {
+			return ActReturn.builder().retCode(RetCode.SUCCESS)
+					.event(new AccountPayOkEvent(cmd.getAccountCode(), cmd.getAmt())).build();
+		}
+		else {
+			String retMessage = String.format("余额不足，剩余:%f, 当前需要:%f", amt, cmd.getAmt());
+			return ActReturn.builder().retCode(RetCode.FAIL).retMessage(retMessage)
+					.event(new AccountPayFailEvent(cmd.getAccountCode(), cmd.getAmt(), retMessage)).build();
+		}
+	}
+
+	public void on(AccountPayOkEvent event) {
+		amt -= event.getAmt();
+	}
+
+	public void on(AccountPayFailEvent event) {
+	}
+
+	@CommandHandler(aggregateRootId = "accountCode")
+	public ActReturn act(AccountPayCompensateCmd cmd) {
+		return ActReturn.builder().retCode(RetCode.SUCCESS)
+				.event(new AccountPayCompensateOkEvent(cmd.getAccountCode(), cmd.getAmt())).build();
+	}
+
+	public void on(AccountPayCompensateOkEvent event) {
+		amt += event.getAmt();
+	}
+
 }
--- shopping-cart/domain/src/main/java/com/iquantex/samples/shopping/domain/GoodsAggregate.java
+++ shopping-cart/domain/src/main/java/com/iquantex/samples/shopping/domain/GoodsAggregate.java
@@ -2,10 +2,23 @@ package com.iquantex.samples.shopping.domain;
 
 import com.iquantex.phoenix.core.message.RetCode;
 import com.iquantex.phoenix.server.aggregate.ActReturn;
+import com.iquantex.phoenix.server.aggregate.CommandHandler;
 import com.iquantex.phoenix.server.aggregate.EntityAggregateAnnotation;
 import com.iquantex.phoenix.server.aggregate.QueryHandler;
-import com.iquantex.samples.shopping.goods.GoodsQueryCmd;
-import com.iquantex.samples.shopping.goods.GoodsQueryEvent;
+import com.iquantex.samples.shopping.coreapi.goods.GoodsCancelCmd;
+import com.iquantex.samples.shopping.coreapi.goods.GoodsCancelOkEvent;
+import com.iquantex.samples.shopping.coreapi.goods.GoodsConfirmCmd;
+import com.iquantex.samples.shopping.coreapi.goods.GoodsConfirmOkEvent;
+import com.iquantex.samples.shopping.coreapi.goods.GoodsQueryCmd;
+import com.iquantex.samples.shopping.coreapi.goods.GoodsQueryEvent;
+import com.iquantex.samples.shopping.coreapi.goods.GoodsSellCmd;
+import com.iquantex.samples.shopping.coreapi.goods.GoodsSellCompensateCmd;
+import com.iquantex.samples.shopping.coreapi.goods.GoodsSellCompensateOkEvent;
+import com.iquantex.samples.shopping.coreapi.goods.GoodsSellFailEvent;
+import com.iquantex.samples.shopping.coreapi.goods.GoodsSellOkEvent;
+import com.iquantex.samples.shopping.coreapi.goods.GoodsTryCmd;
+import com.iquantex.samples.shopping.coreapi.goods.GoodsTryFailEvent;
+import com.iquantex.samples.shopping.coreapi.goods.GoodsTryOkEvent;
 
 import java.io.Serializable;
 
@@ -31,4 +44,81 @@ public class GoodsAggregate implements Serializable {
 		return ActReturn.builder().retCode(RetCode.SUCCESS)
 				.event(new GoodsQueryEvent(cmd.getGoodsCode(), qty, frozenQty)).build();
 	}
+
+	@CommandHandler(aggregateRootId = "goodsCode")
+	public ActReturn act(GoodsTryCmd cmd) {
+		if (qty - frozenQty > cmd.getFrozenQty()) {
+			return ActReturn.builder().retCode(RetCode.SUCCESS)
+					.event(new GoodsTryOkEvent(cmd.getGoodsCode(), cmd.getFrozenQty())).build();
+		}
+		else {
+			String retMessage = String.format("商品可用不足，剩余:%d, 当前需要冻结:%d", qty - frozenQty, cmd.getFrozenQty());
+			return ActReturn.builder().retCode(RetCode.FAIL).retMessage(retMessage)
+					.event(new GoodsTryFailEvent(cmd.getGoodsCode(), cmd.getFrozenQty(), retMessage)).build();
+		}
+	}
+
+	public void on(GoodsTryOkEvent event) {
+		frozenQty += event.getFrozenQty();
+	}
+
+	public void on(GoodsTryFailEvent event) {
+	}
+
+	@CommandHandler(aggregateRootId = "goodsCode")
+	public ActReturn act(GoodsConfirmCmd cmd) {
+		return ActReturn.builder().retCode(RetCode.SUCCESS)
+				.event(new GoodsConfirmOkEvent(cmd.getGoodsCode(), cmd.getFrozenQty())).build();
+	}
+
+	public void on(GoodsConfirmOkEvent event) {
+		qty -= event.getFrozenQty();
+		frozenQty -= event.getFrozenQty();
+
+	}
+
+	@CommandHandler(aggregateRootId = "goodsCode")
+	public ActReturn act(GoodsCancelCmd cmd) {
+		return ActReturn.builder().retCode(RetCode.SUCCESS)
+				.event(new GoodsCancelOkEvent(cmd.getGoodsCode(), cmd.getFrozenQty())).build();
+	}
+
+	public void on(GoodsCancelOkEvent event) {
+		frozenQty -= event.getFrozenQty();
+
+	}
+
+	@CommandHandler(aggregateRootId = "goodsCode")
+	public ActReturn act(GoodsSellCmd cmd) {
+		if (cmd.getQty() < 0) {
+			throw new RuntimeException("数不能小于0");
+		}
+		if (qty > cmd.getQty()) {
+			return ActReturn.builder().retCode(RetCode.SUCCESS)
+					.event(new GoodsSellOkEvent(cmd.getGoodsCode(), cmd.getQty())).build();
+		}
+		else {
+			String ret = String.format("余额不足，剩余:%d, 当前需要:%d", qty, cmd.getQty());
+			return ActReturn.builder().retCode(RetCode.FAIL).retMessage(ret)
+					.event(new GoodsSellFailEvent(cmd.getGoodsCode(), cmd.getQty(), ret)).build();
+		}
+	}
+
+	public void on(GoodsSellOkEvent event) {
+		qty -= event.getQty();
+	}
+
+	public void on(GoodsSellFailEvent event) {
+	}
+
+	@CommandHandler(aggregateRootId = "goodsCode")
+	public ActReturn act(GoodsSellCompensateCmd cmd) {
+		return ActReturn.builder().retCode(RetCode.SUCCESS)
+				.event(new GoodsSellCompensateOkEvent(cmd.getGoodsCode(), cmd.getQty())).build();
+	}
+
+	public void on(GoodsSellCompensateOkEvent event) {
+		qty += event.getQty();
+	}
+
 }
